5d1bacbe8cec0d57e05aa7419626d186e898e1e8
Skipper <155382339+Sklpp3r@users.noreply.github.com>
2025-01-23 23:24:56 +0300

    map

--- Assets/Scripts/Player/Hook.cs
+++ Assets/Scripts/Player/Hook.cs
@@ -42,7 +42,7 @@ public class GrapplingGun : MonoBehaviour {
             joint.minDistance = distanceFromPoint * 0.25f;
 
             //Adjust these values to fit your game.
-            joint.spring = 100f;
+            joint.spring = 50f;
             joint.damper = 7f;
             joint.massScale = 4.5f;
 
